93050c0abc67814f0303b0d3511ea1a85c46a685
Sneha Kumari <sneha057btece22@igdtuw.ac.in>
2025-06-29 18:55:35 +0530

    Add render-build.sh to install Chromium

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 # This script installs Chromium for Puppeteer on Render
+
+# Install dependencies
 npm install
-npx puppeteer browsers install chrome
+
+# Make puppeteer executable
+chmod +x ./node_modules/.bin/puppeteer
+
+# Install Chromium
+npx puppeteer --yes browsers install chrome
