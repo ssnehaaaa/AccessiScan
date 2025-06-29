6d4aeda87e066e10b59f709e9a9e85cfebe7a9b4
Sneha Kumari <sneha057btece22@igdtuw.ac.in>
2025-06-29 19:18:13 +0530

    fix: corrected puppeteer install in render-build.sh

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,11 +1,2 @@
-#!/usr/bin/env bash
-# This script installs Chromium for Puppeteer on Render
-
-# Install dependencies
 npm install
-
-# Make puppeteer executable
-chmod +x ./node_modules/.bin/puppeteer
-
-# Install Chromium
-npx puppeteer --yes browsers install chrome
+npx puppeteer browsers install chrome
